44e1a02835fd38630bc4e8bf79fd49d06a15b81a
Ava <avamsteimle@gmail.com>
2022-03-16 20:07:16 -0700

    more stlying

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,33 +1,47 @@
 import React from 'react';
 
+const styles = {
+  h1: {
+    fontSize: "50px",
+    textAlign: "center",
+  },
+  btn: {
+    padding: "10px",
+    margin: "10px"
+  }
 
+}
 export default function Contact() {
+  const handleSubmit = () => {
+    
+  }
     return (
       <div>
-          <h1>CONTACT ME.</h1>
+          <h1
+          style={styles.h1}>
+            CONTACT ME.</h1>
           <section id="contactMe" className="contactMe d-flex justify-content-center">
-            <button className="contactDiv gmail btn btn-dark">
-                <img src="./src/images/github.png" alt="gmail icon link" className="contactImg" />
+            <button 
+              style={styles.btn}
+              className="contactDiv gmail btn btn-dark">
+                Gmail
             </button>
-            <button className="contactDiv gitHub btn btn-dark">
-                <img src="./images/github.png" alt="GitHub icon link" className="contactImg" />
-            </button>
-            <button className="contactDiv linkedIn btn btn-dark">
-                <img src="./images/linked.png" alt="LinkedIn icon link" className="contactImg" />
-            </button> 
+            <a
+              href="https://github.com/kreativekntrl"
+              style={styles.btn} 
+              className="contactDiv gitHub btn btn-dark">
+                GitHub
+            </a>
+            <a 
+              href="https://www.linkedin.com/in/avasteimle/"
+              style={styles.btn} 
+              className="contactDiv linkedIn btn btn-dark">
+                LinkedIn
+            </a> 
 
         </section>
 
-    <button className="btn btn-dark">
-        <a
-          href="http://github.com/kreativekntrl"
-          onClick={() => window.location.href('http://github.com/kreativekntrl')}
-        >
-          About Me
-        </a>
-      </button>
-
-        <form className="contact-form bg-light">
+  <form className="contact-form bg-light">
   <div className="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="JeffBezos@gmail.com" />
@@ -36,13 +50,16 @@ export default function Contact() {
 
   <div className="form-group">
     <label for="textInput">Note</label>
-    <input type="text" className="form-control" id="textInput" placeholder="Howdy Ava, I would like to let you know I am putting you in my will ! ;-)" />
+    <input type="text" className="form-control" id="textInput" placeholder="Hey Ava.." />
   </div>
 
-  <button type="submit" className="btn btn-dark">Submit</button>
+  <button
+    style={styles.btn} 
+    type="submit" 
+    className="btn btn-dark"
+    onClick={handleSubmit}>Submit</button>
 </form>
-
-      </div>
+</div>
     );
   }
   
